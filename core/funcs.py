f6944919a6cb2889b0b1c0fb444287a29838ce09
dev0ps221 <seybaxscorpion@gmail.com>
2022-10-18 05:12:57 +0000

    initial actions : docopy

--- core/funcs.py
+++ core/funcs.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
 
 class XSCPFUNCS:
+    def get_keydata_from(self,src,key,ifnot):
+        return src[key] if key in src else ifnot
     def docopy(self,data={}):
-        sources = data['sources'] if 'sources' in data else []
-        dest = data['dest'] if 'dest' in data else None
+        sources = self.get_keydata_from(data,'sources',[])
+        dest = self.get_keydata_from(data,'dest',None)
+        host = self.get_keydata_from(data,'host',None)
+        user = self.get_keydata_from(data,'user',None)
+        pwd = self.get_keydata_from(data,'pwd',None)
+        if len(sources) and dest and host and usr and pwd:
+            actionstr = 
